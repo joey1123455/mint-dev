225d67a1c4fa5ebe7c4207e15bba37f71c546a26
legionOfGo <folayanjoey@gmail.com>
2024-01-19 00:53:38 +0100

    vendor and customer model

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,21 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=200)
 
-
-
     def __str__(self):
         return self.user.username
+    
+
+class Customer(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    # Add other customer-specific fields here
+
+    def __str__(self):
+        return f"Customer - {self.profile.user.username}"
+    
+
+class Vendor(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    # Add other vendor-specific fields here
+
+    def __str__(self):
+        return f"Vendor - {self.profile.user.username}"
