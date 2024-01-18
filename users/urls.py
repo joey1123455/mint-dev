64cab596af73ceca2ad391fc6d40d6b4073441e4
legionOfGo <folayanjoey@gmail.com>
2024-01-18 19:13:11 +0100

    implemented registration

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+# myapp/urls.py
+from django.urls import path
+from .views import UserRegistrationView
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+]
---
+++ users/views.py
@@ -0,0 +1,16 @@
+# Create your views here.
+# myapp/views.py
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from .serialisers import UserSerializer
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
