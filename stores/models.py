14599dc99ae4c7c0de132634c23c743d0c3c70cd
Kemzzy-Dev <kemzzy17@gmail.com>
2024-01-19 14:29:57 +0100

    Revert "changes"
    This reverts commit d88939bdff780da3d4775e43abab7181a0d03931.

--- stores/models.py
+++ stores/models.py
@@ -10,7 +10,7 @@ class Products(models.Model):
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
