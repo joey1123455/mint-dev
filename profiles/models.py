8b95f35803ce779d26f9b58c0684d38426cd1f95
legionOfGo <folayanjoey@gmail.com>
2024-01-19 01:26:53 +0100

    refactored profile models

--- profiles/models.py
+++ profiles/models.py
@@ -1,33 +1,36 @@
 # profile/models.py
+import uuid
 from django.contrib.auth.models import User
 from django.db import models
 from wallets.models import Wallet
-import uuid
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=200)
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     profile_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     phone_number = models.CharField(max_length=20)
+#     address = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.user.username
+#     def __str__(self):
+#         return self.user.username
     
 
 class Customer(models.Model):
-    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     customer_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     wallet = models.OneToOneField(Wallet, on_delete=models.CASCADE, null=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
 
     def __str__(self):
         return f"Customer - {self.profile.user.username}"
     
 
 class Vendor(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    account_number = models.CharField(max_length=20, unique=True)  # Add an account number field
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=20, unique=True)
     bvn = models.CharField(max_length=11, unique=True)
     vendor_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     wallet = models.OneToOneField(Wallet, on_delete=models.CASCADE, null=True)
--- wallets/models.py
+++ wallets/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 
 class Wallet(models.Model):
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    pin = models.CharField(max_length=4)  # Add a PIN field for simplicity, adjust as needed
-    limit_level = models.PositiveIntegerField(default=1)  # Add a limit level field
+    pin = models.CharField(max_length=4) 
+    limit_level = models.PositiveIntegerField(default=1) 
     wallet_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # Add a UUID wallet ID field
     account_number = models.CharField(max_length=20, unique=True)  # Add an account number field
+    bank = models.CharField(max_length=20)
     bvn = models.CharField(max_length=11, unique=True)  # Add a BVN field
-    # Add other wallet-related fields as needed
 
     def __str__(self):
         return f"Wallet - {self.wallet_id} ({self.user.username})"
