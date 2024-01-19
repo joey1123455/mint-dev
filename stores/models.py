8f5afe19cab7003ac0b8ea3dd5dc8ac604e560a3
Kemzzy-Dev <kemzzy17@gmail.com>
2024-01-19 14:01:31 +0100

    update

--- stores/models.py
+++ stores/models.py
@@ -1,10 +1,18 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Products(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     product_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     description = models.TextField()
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
