04e7570a23d95ab1402b1578aca71415bdcb2874
legionOfGo <folayanjoey@gmail.com>
2024-01-19 18:03:02 +0100

    changed django version

--- squadBackend/settings.py
+++ squadBackend/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+# make changes for deployment
 import os
 from pathlib import Path
 from dotenv import load_dotenv
