7fc8854d942646bfd6983599198446b73fc7086e
legionOfGo <folayanjoey@gmail.com>
2024-01-19 01:07:12 +0100

    added wallets

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 # profile/models.py
 from django.contrib.auth.models import User
 from django.db import models
+from wallets.models import Wallet
 import uuid
 
 class Profile(models.Model):
@@ -17,10 +18,8 @@ class Profile(models.Model):
 
 class Customer(models.Model):
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    account_number = models.CharField(max_length=20, unique=True)  # Add an account number field
-    bvn = models.CharField(max_length=11, unique=True)
     customer_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    # Add other customer-specific fields here
+    wallet = models.OneToOneField(Wallet, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"Customer - {self.profile.user.username}"
@@ -31,7 +30,7 @@ class Vendor(models.Model):
     account_number = models.CharField(max_length=20, unique=True)  # Add an account number field
     bvn = models.CharField(max_length=11, unique=True)
     vendor_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    # Add other vendor-specific fields here
+    wallet = models.OneToOneField(Wallet, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"Vendor - {self.profile.user.username}"
--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Wallet(models.Model):
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    pin = models.CharField(max_length=4)  # Add a PIN field for simplicity, adjust as needed
+    limit_level = models.PositiveIntegerField(default=1)  # Add a limit level field
+    wallet_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # Add a UUID wallet ID field
+    account_number = models.CharField(max_length=20, unique=True)  # Add an account number field
+    bvn = models.CharField(max_length=11, unique=True)  # Add a BVN field
+    # Add other wallet-related fields as needed
+
+    def __str__(self):
+        return f"Wallet - {self.wallet_id} ({self.user.username})"
