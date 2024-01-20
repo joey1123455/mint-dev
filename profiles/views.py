78ced33345b23823f975126665459ab61c9c8bd3
legionOfGo <folayanjoey@gmail.com>
2024-01-20 06:34:00 +0100

    payment

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 from rest_framework.authentication import TokenAuthentication
 
-
+# add env
 from rest_framework import status
 from rest_framework.views import APIView
 from .serilizers import VendorCreateSerializer, VendorViewSerializer, CreateCustomerSerializer, CustomerViewSerializer
